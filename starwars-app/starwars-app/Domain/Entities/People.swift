0c23e6a49bbca63b7082d09e8426641cdff28012
Victor Martins Tinoco - VTN <victormartinstinoco@live.com>
2020-03-04 15:46:59 -0300

    set app delegate and scene delegate

---
+++ starwars-app/starwars-app/Domain/Entities/People.swift
@@ -0,0 +1,20 @@
+//
+//  People.swift
+//  starwars-app
+//
+//  Created by Victor Martins Tinoco - VTN on 04/03/20.
+//  Copyright © 2020 tinoco. All rights reserved.
+//
+
+import Foundation
+
+struct People: Codable {
+    let name: String
+    let height: String
+    let mass: String
+    let hair_color: String
+    let skin_color: String
+    let eye_color: String
+    let birth_year: String
+    let gender: String
+}
