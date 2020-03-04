cb10759df6e2ef88bf5a43f27a54e7f2f30b7b5b
Victor Martins Tinoco - VTN <victormartinstinoco@live.com>
2020-03-04 15:17:46 -0300

    DI

---
+++ starwars-app/starwars-app/Application/DI/AppDIContainer.swift
@@ -0,0 +1,13 @@
+//
+//  AppDIContainer.swift
+//  starwars-app
+//
+//  Created by Victor Martins Tinoco - VTN on 04/03/20.
+//  Copyright © 2020 tinoco. All rights reserved.
+//
+
+import UIKit
+
+class AppDIContainer {
+    
+}
