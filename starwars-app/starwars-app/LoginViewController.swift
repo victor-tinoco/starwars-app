b2e98bf9fabbd83ad3827374c2e5d633f02bd405
Nina Dominique Thomé Bernardo - NBE <nina.bernardo@idesk502iterislocal.local>
2020-03-05 10:03:40 -0300

    criando a controller de login

---
+++ starwars-app/starwars-app/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  starwars-app
+//
+//  Created by Nina Dominique Thomé Bernardo - NBE on 05/03/20.
+//  Copyright © 2020 tinoco. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+}
